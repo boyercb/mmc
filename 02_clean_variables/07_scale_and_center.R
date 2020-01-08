3e8c70eb69985f8a5dde49abbe32dab16d0ff896
boyercb <christopherbboyer@gmail.com>
2020-01-08 13:06:11 -0500

    Create 07_scale_and_center.R

---
+++ 02_clean_variables/07_scale_and_center.R
@@ -0,0 +1,8 @@
+# create centered versions of pre-treatment variables
+el[, paste0(names(el)[names(el) %in% invariant_covariates], "_c")] <- 
+  scale(el[, names(el) %in% invariant_covariates], scale = FALSE)
+
+el_imputed[, paste0(names(el_imputed)[names(el_imputed) %in% invariant_covariates], "_c")] <- 
+  scale(el_imputed[, names(el_imputed) %in% invariant_covariates], scale = FALSE)
+
+# TO ADD: standardize indices to control mean and sd?

7327a430da39b0a0649502ccd208e63683f94706
boyercb <christopherbboyer@gmail.com>
2020-01-13 13:38:14 -0500

    scale and center covariates

--- 02_clean_variables/08_scale_and_center.R
+++ 02_clean_variables/08_scale_and_center.R
@@ -5,4 +5,8 @@ el[, paste0(names(el)[names(el) %in% invariant_covariates], "_c")] <-
 el_imputed[, paste0(names(el_imputed)[names(el_imputed) %in% invariant_covariates], "_c")] <- 
   scale(el_imputed[, names(el_imputed) %in% invariant_covariates], scale = FALSE)
 
+pooled_el[, paste0(names(pooled_el)[names(pooled_el) %in% invariant_covariates], "_c")] <- 
+  scale(pooled_el[, names(pooled_el) %in% invariant_covariates], scale = FALSE)
+
+
 # TO ADD: standardize indices to control mean and sd?

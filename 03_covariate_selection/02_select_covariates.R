22c145e982227b8ae8306e778f61e0d1c0e6aa78
boyercb <christopherbboyer@gmail.com>
2020-01-15 10:40:24 -0500

    add pooled dataset covariate selection

--- 03_covariate_selection/02_select_covariates.R
+++ 03_covariate_selection/02_select_covariates.R
@@ -20,6 +20,24 @@ if (run_lasso) {
 		na = ""
 	)
 	
+	selected_pooled_covariates <-
+	  outcomes %>%
+	  map(function (x) {
+	    select_covariates(
+	      outcome_name = x,
+	      covariates = paste0(invariant_covariates, "_c"), # use centered versions 
+	      data = pooled_el,
+	      sims = lasso_sims,
+	      N_folds = lasso_folds
+	    )
+	  }) %>% bind_rows()
+	
+	write.csv(
+	  selected_pooled_covariates,
+	  "03_covariate_selection/selected_pooled_covariates.csv",
+	  na = ""
+	)
+	
 	
 } else {
   selected_covariates <-
@@ -27,5 +45,11 @@ if (run_lasso) {
 			"03_covariate_selection/selected_covariates.csv",
 			stringsAsFactors = FALSE
 		)
+  
+  selected_pooled_covariates <-
+    read.csv(
+      "03_covariate_selection/selected_pooled_covariates.csv",
+      stringsAsFactors = FALSE
+    )
 	
 }

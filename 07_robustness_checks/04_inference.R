02d2c2bc09630b43d01818abae0d4ba150279fd2
boyercb <christopherbboyer@gmail.com>
2020-01-16 22:34:01 -0500

    add ri

--- 07_robustness_checks/04_inference.R
+++ 07_robustness_checks/04_inference.R
@@ -1,4 +1,4 @@
-# All blocks (N = 16) -----------------------------------------------------
+# variance estimators -----------------------------------------------------
 
 se_outcomes <- rep(violence_outcomes, each = 9)
 se_types <-
@@ -31,22 +31,57 @@ inference_models <-
          )
        })
 
+
+# randomization inference -------------------------------------------------
+
+ri_models <-
+  map(violence_outcomes,
+      function (x) {
+        ri(
+          outcome = x,
+          covariates = get_covariates(x, selected_covariates),
+          data = el_imputed,
+          cluster = "block_id",
+          sims = sims
+        )
+      })
+
+
+
+
+# compile results ---------------------------------------------------------
+
 inference_vcovs <- lapply(inference_models, get, x = "robust")
+ri_pvals <- sapply(ri_models, get, x = "ri_pval")
 
 inference_table <-
   tibble(
-    outcome = se_outcomes,
-    se_type = se_types,
-    std.error = sapply(inference_vcovs, "[", 2, 2),
-    p.value = sapply(inference_vcovs, "[", 2, 4)
+    outcome = c(se_outcomes, violence_outcomes),
+    se_type = c(se_types, rep("ri", 3)),
+    std.error = c(sapply(inference_vcovs, "[", 2, 2), rep(NA, 3)),
+    p.value = c(sapply(inference_vcovs, "[", 2, 4), ri_pvals)
   )
+
+se_order <- c(
+  "HC0",
+  "HC1",
+  "HC2",
+  "HC3",
+  "wild-rademacher",
+  "wild-mammen",
+  "wild-norm",
+  "wild-webb",
+  "xy",
+  "ri"
+)
 
 inference_table <-
   inference_table %>%
   gather(key, value, -outcome, -se_type) %>%
   spread(outcome, value) %>%
   arrange(se_type, desc(key)) %>%
-  mutate_if(is.numeric, function(x) as.character(specd(x, 3))) %>%
+  mutate_if(is.numeric, function(x) ifelse(is.na(x), "-", as.character(specd(x, 3)))) %>%
+  arrange(match(se_type, se_order)) %>%
   mutate(
     ipv_control_2pl_irt_index_w =
       ifelse(
@@ -71,7 +106,20 @@ inference_table <-
         key == "p.value",
         "",
         se_type
-      )
+      ),
+    se_type = case_when(
+      se_type == "HC0" ~ "CR0",
+      se_type == "HC1" ~ "CR1",
+      se_type == "HC2" ~ "CR2",
+      se_type == "HC3" ~ "CR3",
+      se_type == "wild-rademacher" ~ "wild-cluster bootstrap-se (Rademacher)",
+      se_type == "wild-mammen" ~ "wild-cluster bootstrap-se (Mammen)",
+      se_type == "wild-norm" ~ "wild-cluster bootstrap-se (Norm)",
+      se_type == "wild-webb" ~ "wild-cluster bootstrap-se (Webb)",
+      se_type == "xy" ~ "cluster pairs bootstrap-se",
+      se_type == "ri" ~ "randomization inference $\\beta$",
+      se_type == "ri" ~ "randomization inference $t$"
+    )
   ) %>%
   select(
     se_type,
@@ -81,11 +129,9 @@ inference_table <-
   )
 
 
-# randomization inference -------------------------------------------------
+# make tables -------------------------------------------------------------
 
-
-
-# Make tables -------------------------------------------------------------
+options(knitr.kable.NA = '')
 
 sink("08_memo/tables/inference.tex")
 kable(
@@ -98,46 +144,14 @@ kable(
   linesep = c("", "\\addlinespace")
 ) %>% 
   kable_styling(latex_options = "HOLD_position") %>%
-  add_header_above(c(" ", "(1)", "(2)", "(3)"), line = F)
+  add_header_above(c(" ", "(1)", "(2)", "(3)"), line = F) %>%
+  footnote("Comparison of inference from sampling and randomization-based 
+       uncertainty estimates for pre-registered primary outcomes. Estimated standard errors
+       are shown with $p$-value from two-sided hypothesis of no effect shown in parentheses. 
+       CR0 - CR3 are cluster and heteroskedasticity robust variance estimators as defined in
+       The wild cluster bootstrap estimates use the algorithm from Cameron, Gelbach, & Miller (2008) 
+       with multipliers drawn from either the Rademacher, Mammen, Normal, or Webb distributions.", 
+       escape = F, threeparttable = T )
 sink()
-
-# kable
-# texreg(
-#   lapply(main_models, get, x = "fit"),
-#   custom.model.names = c("IPV",
-#                          "IPV",
-#                          "Physical/Sexual",
-#                          "Physical/Sexual",
-#                          "Emotional",
-#                          "Emotional"),
-#   custom.coef.names = c("Constant", "MMC"),
-#   override.se = lapply(lapply(main_models, get, x = "robust"), "[", , 2),
-#   override.pvalues = lapply(lapply(main_models, get, x = "robust"), "[", , 4),
-#   reorder.coef = c(2, 1),
-#   custom.gof.rows = list(
-#     "Covariates" = paste0("\\textrm{", primary_covariates, "}"),
-#     "Clusters" = c(16, 16, 16, 16, 16, 16)
-#   ),  
-#   reorder.gof = c(1, 2, 4, 3),
-#   custom.gof.names = c("Adj. R$^2$", "Observations"),
-#   omit.coef = "_[c]",
-#   digits = 3,
-#   include.ci = FALSE,
-#   table = FALSE,
-#   include.rsquared = FALSE,
-#   include.rmse = FALSE,
-#   booktabs = TRUE,
-#   use.packages = FALSE,
-#   custom.note = "\\parbox{\\linewidth}{\\vspace{2pt} 
-#        \\textit{Notes:} Estimates of the intent-to-treat effects of Modern Man mobile 
-#        messaging program on pre-registered primary outcomes using adjusted regression 
-#        specification based on the Lin 2013 estimator with wild cluster bootstrap 
-#        standard errors in parentheses. Columns 1 and 2 are a composite index of 
-#        acts of intimate partner violence. Columns 3 and 4 are a composite index of acts
-#        of physical violence. Columns 5 and 6 are a composite index of acts of sexual violence.
-#        All indices were constructed using the first factor from IRT models of subitems. 
-#        Bootstrapped standard errors estimated using 10,000 replicates. \\\\ %stars.}"
-# ) %>% print()
-# sink()
 
 
